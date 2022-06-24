213ea28418355391d9f4459e6263c26d38acd9ee
elenaB1864 <alina.balanescu1864@gmail.com>
2022-06-24 20:10:21 +0100

    Added the git hub link for the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <header>
+        {" "}
+        This website was coded by{" "}
+        <a
+          href="https://github.com/elenaB1864"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Elena Balanescu
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/elenaB1864/weather-application-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced.
+        </a>
+      </header>
+    </div>
+  );
 }
